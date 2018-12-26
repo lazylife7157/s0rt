4721bd7a07c152ea4548b7a31e961b0cafb2fce3
lazylife7157 <lazylife7157@gmail.com>
2018-12-26 22:13:20 +0900

    Support file output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate structopt;
 
+use std::io::{self, Write};
 use exitfailure::ExitFailure;
 
 use s0rt::algorithms::stalin;
@@ -14,7 +15,7 @@ struct Opt {
     algorithm: String,
 
     /// Write result to file instead of standard output
-    #[structopt(short = "o", long = "output", parse(from_os_str))]
+    #[structopt(short = "o", long = "output", parse(from_os_str), default_value = "")]
     output: PathBuf,
 
     /// Files to process
@@ -29,10 +30,25 @@ fn main() -> Result<(), ExitFailure> {
         _ => stalin::sort
     };
 
+    let stdout = io::stdout();
+    let mut handle: io::BufWriter<Box<Write>> = match opt.output.to_str() {
+        Some(path) if path != "" => {
+            let file = std::fs::File::open(path)?;
+            io::BufWriter::new(Box::new(file))
+        }
+        _ => {
+            let locked_stdout = stdout.lock();
+            io::BufWriter::new(Box::new(locked_stdout))
+        }
+    };
+
     for file in opt.files.iter() {
         let strings: String = std::fs::read_to_string(file)?;
         let lines: Vec<String> = strings.lines().map(String::from).collect();
-        println!("{}", sort(&lines).join("\n"))
+
+        for line in sort(&lines) {
+            writeln!(handle, "{}", line)?;
+        }
     }
     Ok(())
 }
